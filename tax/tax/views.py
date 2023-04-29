cb81a75d59663e92874d8297f9217d52c68c48dd
Minn <83846950+Fahad15a@users.noreply.github.com>
2023-04-29 18:05:51 +0300

    new

---
+++ tax/tax/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse("<h1>This is a site to calculate tax</h1>")
+def calculate_tax(request, num):
+    tax_rate = 0.15
+    total = num * (1 + tax_rate)
+    return HttpResponse(f"<h1>The total price after tax is {total}</h1>")
+def tax_rate(request):
+    tax_rate = 0.15
+    return render(request, 'tax_rate.html', {'tax_rate': tax_rate})
